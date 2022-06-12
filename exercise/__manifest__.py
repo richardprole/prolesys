cff6d82cbe5406b6ffd6900278f555f56b85f681
Leonardo Solano <richard@prolesys.com>
2022-06-12 19:33:41 +0000

    [ADD] Exercise Part 1

---
+++ exercise/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf* -*-
+
+{
+    'name': 'Odoo Exercise',
+    
+    'summary': """Exercise to do Training """,
+    'description':"""
+    Exercise will do:
+        - Add an to do list
+        - Create notes
+        - Add users
+        """,
+    
+    'author': 'Leonardo Solano',
+    
+    'website': 'https://www.odoo.com',
+    'catergory': 'Training',
+    'version':  '0.1',
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    'demo': [
+        
+    ],
+}
