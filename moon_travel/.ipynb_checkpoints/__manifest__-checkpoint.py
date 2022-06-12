7fbdb4e86001a829629a53d5afb4e6d396c46b56
Leonardo Solano <richard@prolesys.com>
2022-06-12 19:37:15 +0000

    [ADD] Moon Traveling

---
+++ moon_travel/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Moon Odoo',
+    
+    'summary': """Odoo app to travel to space """,
+    'description':"""
+    Module to manage rocket launching:
+        - Rocket Administrator
+        - Astronauts Logistic
+        - Launch Dates
+        """,
+    
+    'author': 'Leonardo Solano',
+    
+    'website': 'https://www.odoo.com',
+    'catergory': 'Training',
+    'version':  '0.1',
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    'demo': [
+        
+    ],
+}
