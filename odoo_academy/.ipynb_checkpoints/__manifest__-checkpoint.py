917cbb4ccc7b160be42f1d38d546b871d1569141
Leonardo Solano <richard@prolesys.com>
2022-06-12 19:07:14 +0000

    [ADD] Basic Module

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage Training """,
+    'description':"""
+    Academy module to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+        """,
+    
+    'author': 'Leonardo Solano',
+    
+    'website': 'https://www.odoo.com',
+    'catergory': 'Training',
+    'version':  '0.1',
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    'demo': [
+        
+    ],
+}
